aa2d51b3cff8cf81d53c0eaf5e6d618b958ab10f
Camilo Polymeris <camilo@circleci.com>
2018-10-11 20:29:39 -0300

    Versioning with lein-v and deploying to clojars

--- browser-examples/project.clj
+++ browser-examples/project.clj
@@ -1,15 +1,19 @@
-(defproject cljs-aws-browser-examples "0.1.0-SNAPSHOT"
+(defproject cljs-aws-browser-examples :lein-v
   :description "Browser examples for cljs-aws"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.async "0.4.474"]
-                 [cljs-aws "0.1.0-SNAPSHOT"]
-                 [prismatic/dommy "1.1.0"]]
+                 [prismatic/dommy "1.1.0"]
+                 [cljs-aws nil]]
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
-            [lein-npm "0.6.2"]]
+            [lein-npm "0.6.2"]
+            [com.roomkey/lein-v "6.3.0"]]
+  :middleware [leiningen.v/version-from-scm
+               leiningen.v/dependency-version-from-scm
+               leiningen.v/add-workspace-data]
   :npm {:dependencies [[aws-sdk "2.94.0"]]}
   :clean-targets ^{:protect false} ["resources/public/js/compiled"]
   :cljsbuild {:builds
